b2e1fd0cd90683613020c17b8e33be921aef790c
luopan297091659 <luopan297091659@gmail.com>
2026-02-05 19:32:47 +0900

    test

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 // it's recommended to set DB_HOST/DB_USER/DB_PASSWORD/DB_NAME in your environment
 // and rotate passwords after any changes.
 module.exports = mysql.createPool({
-  host: process.env.DB_HOST || '139.196.44.6',
-  // host: process.env.DB_HOST || '127.0.0.1',
+  // host: process.env.DB_HOST || '139.196.44.6',
+  host: process.env.DB_HOST || '127.0.0.1',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '6586156',
   database: process.env.DB_NAME || 'church_db',
